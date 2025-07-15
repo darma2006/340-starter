3014557cf0aff43e14c692636760fdec74eda344
darma2006 <lopezsolisdarmasoley@gmail.com>
2025-07-14 18:40:21 -0600

    week 1

--- server.js
+++ server.js
@@ -5,14 +5,27 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+app.set("view engine", "ejs")
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout") // not at views root
+app.use(static)
+
 app.use(static)
 
 /* ***********************
