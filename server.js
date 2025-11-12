ad7d62da58a9c2cffdab12f2066b75a7b612a0d3
darma2006 <lopezsolisdarmasoley@gmail.com>
2025-11-12 09:51:24 -0600

    week 3

--- server.js
+++ server.js
@@ -6,31 +6,58 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+const staticRoutes = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/") 
+
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+app.use(staticRoutes)
+
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
 })
 
-app.use(static)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
